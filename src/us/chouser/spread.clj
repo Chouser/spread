03aa70ad1fdfcc202d1ebf015019c999de70424e
Chouser <chouser@n01se.net>
2022-08-18 23:14:52 -0400

    Change spread syntax from ... to ~@

--- src/us/chouser/spread.clj
+++ src/us/chouser/spread.clj
@@ -2,34 +2,27 @@
 
 (def mapcat-identity (mapcat identity))
 
-(defn add-to-maps [maps k v]
+(defn ^:private add-to-maps [maps k v]
   (if (map? (peek maps))
     (conj (pop maps) (assoc (peek maps) k v))
     (conj maps {k v})))
 
 (defn ^:private build-map [form key-fn]
-  (loop [[k & [maybe-val :as more]] (rest form), maps []]
-    (if k
-      (if (symbol? k)
-        (if-let [[_ suffix] (re-matches #"\.\.\.(.*)" (str k))]
-          (if (empty? suffix)
-            (if (seq more)
-              (recur (rest more) (conj maps maybe-val))
-              (throw (ex-info "No expression supplied after ellipsis"
-                              {:form form})))
-            (recur more (conj maps (symbol suffix))))
-          (if (namespace k)
-            (throw (ex-info
-                    (format "Namespaced symbol keys not yet supported: %s" k)
-                    {:key k :form form}))
-            (recur more (add-to-maps maps (key-fn k) k))))
-        (if (seq more)
-          (recur (rest more) (add-to-maps maps k maybe-val))
-          (throw (ex-info (format "No value supplied for key %s" (pr-str k))
-                          {:key k :form form}))))
-      (if (next maps)
-        `(into ~(first maps) mapcat-identity [~@(rest maps)])
-        (first maps)))))
+  (loop [[k & [maybe-val :as more] :as args] (rest form), maps []]
+    (cond
+      (empty? args) (if (next maps)
+                      `(into ~(first maps) mapcat-identity [~@(rest maps)])
+                      (first maps))
+      (symbol? k) (if (namespace k)
+                    (throw (ex-info
+                            (format "Namespaced symbol keys not yet supported: %s" k)
+                            {:key k :form form}))
+                    (recur more (add-to-maps maps (key-fn k) k)))
+      (and (seq? k)
+           (= `unquote-splicing (first k))) (recur more (conj maps (second k)))
+      (seq more) (recur (rest more) (add-to-maps maps k maybe-val))
+      :else (throw (ex-info (format "No value supplied for key %s" (pr-str k))
+                            {:key k :form form})))))
 
 (defmacro k.    [& args] (build-map &form keyword))
 (defmacro keys. [& args] (build-map &form keyword))
--- test/io/chouser/spread_test.clj
+++ test/io/chouser/spread_test.clj
@@ -10,38 +10,34 @@
 (deftest basic-keywords
   (is (= {:a a} (k. a)))
   (is (= {:a a :b b} (k. a b)))
-  (is (= (merge {:a a :b b} mef) (k. a ...mef b)))
-  (is (= (merge mcd {:a a} mef) (k. ... mcd a ...mef)))
-  (is (= (merge {:h "hotel"} mef) (k. :h "hotel" ... mef)))
-  (is (= {:h "hotel" :e 3} (k. :h "hotel" ...(dissoc mef :f))))
-  (is (= mef (k. :e 9 ...mef)))
-  (is (= {:e 9 :f 4} (k. ...mef :e 9))))
+  (is (= (merge {:a a :b b} mef) (k. a ~@mef b)))
+  (is (= (merge mcd {:a a} mef) (k. ~@ mcd a ~@mef)))
+  (is (= (merge {:h "hotel"} mef) (k. :h "hotel" ~@ mef)))
+  (is (= {:h "hotel" :e 3} (k. :h "hotel" ~@(dissoc mef :f))))
+  (is (= mef (k. :e 9 ~@mef)))
+  (is (= {:e 9 :f 4} (k. ~@mef :e 9))))
 
 (deftest all-macros
-  (is (= (merge {:a a :b b} mef) (k.    a ...mef b)))
-  (is (= (merge {:a a :b b} mef) (keys. a ...mef b)))
-  (is (= (merge {"a" a "b" b} mef) (strs. a ...mef b)))
-  (is (= (merge {'a a 'b b} mef) (syms. a ...mef b))))
+  (is (= (merge {:a a :b b} mef) (k.    a ~@mef b)))
+  (is (= (merge {:a a :b b} mef) (keys. a ~@mef b)))
+  (is (= (merge {"a" a "b" b} mef) (strs. a ~@mef b)))
+  (is (= (merge {'a a 'b b} mef) (syms. a ~@mef b))))
 
 (deftest simplified
   (is (= '{:a a :b b} (macroexpand '(k. a b))))
   (is (= '{:a a :h "hotel"} (macroexpand '(k. :h "hotel", a))))
-  (is (= 'mcd (macroexpand '(k. ... mcd))))
-  (is (= 'mcd (macroexpand '(k. ...mcd)))))
+  (is (= 'mcd (macroexpand '(k. ~@ mcd))))
+  (is (= 'mcd (macroexpand '(k. ~@mcd)))))
 
 (deftest combinations
   (dotimes [_ 100]
-    (let [words (->> '[[a] [b] [... mcd] [...mef] [:g "golf"] [:h "hotel"]]
+    (let [words (->> '[[a] [b] [~@mcd] [~@mef] [:g "golf"] [:h "hotel"]]
                      (sort-by (fn [_] (rand)))
                      (apply concat))]
       (is (= (merge {:a a :b b :g "golf" :h "hotel"} mcd mef)
              (eval (list* 'k. words)))))))
 
 (deftest errors
-  (is (thrown-with-msg? Exception #"No expression"
-                        (#'s/build-map '(k. ...) keyword)))
-  (is (thrown-with-msg? Exception #"No expression"
-                        (#'s/build-map '(k. a b ...) keyword)))
   (is (thrown-with-msg? Exception #"No value supplied"
                         (#'s/build-map '(k. a :x) keyword)))
   (is (thrown-with-msg? Exception #"not yet supported"
